c4cb790c0cc9e88c48c8cb235051a22dc121f50c
bgelov <belov@ymail.com>
2023-06-06 16:45:56 -0300

    Change directory

---
+++ Parsing/hackerrank-cut.sh
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/text-processing-cut-2/problem
+cut -c 2,7
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-3/problem
+cut -c 2-7
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-4/problem
+cut -c-4
+
+
+# https://www.hackerrank.com/challenges/text-processing-cut-5/problem
+cut -f 1-3
