c8959f3ab9b58177395a83b1555132f12823dc3f
bgelov <belov@ymail.com>
2023-06-07 00:04:21 -0300

    Update hackerrank-for.sh

--- Basic/For-Do-Done/hackerrank-for.sh
+++ Basic/For-Do-Done/hackerrank-for.sh
@@ -3,3 +3,13 @@ for num in {1..50}
 do
 echo $num
 done
+
+
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem
+for num in {1..99}
+do
+    if [[ $num%2 -ne 0 ]]
+    then
+        echo $num
+    fi
+done
