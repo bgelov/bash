541a758160d0f126e16516be1a30d36974886124
bgelov <belov@ymail.com>
2023-06-18 22:43:57 -0300

    Add sort and uniq

--- Basic/sort/hackerrank-sort-text.sh
+++ Basic/sort/hackerrank-sort-text.sh
@@ -1,17 +1,21 @@
 # https://www.hackerrank.com/challenges/text-processing-sort-1/problem
 sort
 
+
 # https://www.hackerrank.com/challenges/text-processing-sort-2/problem
 # Sort reverse
 sort -r
+
 
 # https://www.hackerrank.com/challenges/text-processing-sort-3/problem
 # Sort numeric
 sort -n
 
+
 # https://www.hackerrank.com/challenges/text-processing-sort-4/problem
 # Sort numeric desc
 sort -nr
+
 
 # https://www.hackerrank.com/challenges/text-processing-sort-5/problem
 # Revers sort 2 numeric column with tab delimetr
@@ -20,3 +24,12 @@ sort -rnk 2 -t $'\t'
 # -n - Numerical sort
 # -k - Column ordering
 # -t - Tab separted indicator
+
+
+# https://www.hackerrank.com/challenges/text-processing-sort-6/problem
+sort -nk 2 -t $'\t'
+
+
+# https://www.hackerrank.com/challenges/text-processing-sort-7/problem
+# with pipe delimetr
+sort -rnk 2 -t $'|'
