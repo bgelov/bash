c228e55e6d5d8140bc773559d035df8927aee92a
bgelov <belov@ymail.com>
2023-06-18 22:10:29 -0300

    Create hackerrank-read-text.sh

---
+++ Basic/sort/hackerrank-read-text.sh
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/text-processing-sort-1/problem
+sort
+
+# https://www.hackerrank.com/challenges/text-processing-sort-2/problem
+# Sort reverse
+sort -r
+
+# https://www.hackerrank.com/challenges/text-processing-sort-3/problem
+# Sort numeric
+sort -n
+
+# https://www.hackerrank.com/challenges/text-processing-sort-4/problem
+# Sort numeric desc
+sort -nr
+
+# https://www.hackerrank.com/challenges/text-processing-sort-5/problem
+# Revers sort 2 numeric column with tab delimetr
+sort -rnk 2 -t $'\t'
+# -r - For reverse order
+# -n - Numerical sort
+# -k - Column ordering
+# -t - Tab separted indicator
